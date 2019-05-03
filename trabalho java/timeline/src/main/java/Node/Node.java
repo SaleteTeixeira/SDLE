b90cd7143a9359928fc88e9635e25a75749043eb
SaleteTeixeira <salete.diasteixeira@hotmail.com>
2019-05-03 21:46:31 +0100

    criado menu e feitas algumas opções

--- trabalho java/timeline/src/main/java/Node/Node.java
+++ trabalho java/timeline/src/main/java/Node/Node.java
@@ -29,7 +29,6 @@ public class Node implements Serializable {
     private Map<String, List<Post>> waitingListSubsPost;
 
     private Node(String username, String RSA, String host, int port) {
-        //todo (diogo): ir buscar a key e o IP
         this.client = new Client(username, RSA, port, Address.from(host, port));
         this.myPosts = new ArrayList<Post>();
         this.causalID = 1;
@@ -48,12 +47,47 @@ public class Node implements Serializable {
         return this.neighbors;
     }
 
+    public Map<String, Client> getSubs(){
+        return this.subs;
+    }
+
     public void setNeighbors(List<Client> neighbors) {
         this.neighbors = neighbors;
     }
 
     public void setIP(String ip) {
         this.client.setAddress(Address.from(ip, this.client.getPort()));
+    }
+
+    public List<Client> listNeighbors() {
+        List<Client> result = new ArrayList<>();
+
+        for(Client c: this.neighbors){
+            if(!this.subs.containsKey(c.getKey())) result.add(c);
+        }
+
+        return result;
+    }
+
+    public void addSubscriber(Client client) {
+        this.subs.put(client.getKey(), client);
+    }
+
+    public List<Client> listSubscribers(){
+        return new ArrayList<>(this.subs.values());
+    }
+
+    public void removeSubscriber(String key){
+        this.subs.remove(key);
+        this.subsPosts.remove(key);
+        this.causalIdSubs.remove(key);
+        this.waitingListSubsPost.remove(key);
+    }
+
+    public void post(String p){
+        Post post = new Post(p, this.causalID);
+        this.myPosts.add(post);
+        this.causalID++;
     }
 
     public String toString() {
@@ -102,10 +136,7 @@ public class Node implements Serializable {
         }
     }
 
-    private static Node loadState(String username, String RSA, int port, String fileName) {
-        String host = Util.getPublicIp();
-
-
+    private static Node loadState(String username, String RSA, String host, int port, String fileName) {
         Node node = new Node(username, RSA, host, port);
 
         try {
@@ -121,20 +152,100 @@ public class Node implements Serializable {
         return node;
     }
 
+    private static int showMenu() {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Escolha uma das seguintes opções: ");
+        System.out.println("1. Publicar.");
+        System.out.println("2. Ver timeline.");
+        System.out.println("3. Subscrever.");
+        System.out.println("4. Des-subscrever.");
+        System.out.println("5. Logout.");
+
+        return scan.nextInt();
+    }
+
+    private static void post(Node node, String fileName){
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Escreva, em uma linha, o que pretende publicar.");
+
+        String post = scan.nextLine();
+        node.post(post);
+
+        storeState(node, fileName);
+    }
+
+    private static void subscribe(Node node, String fileName){
+        Scanner scan = new Scanner(System.in);
+        int op;
+
+        System.out.println("Escolha uma das seguintes opções: ");
+        System.out.println("1. Apresentar lista de vizinhos que ainda não segue.");
+        System.out.println("2. Adicionar subscritor à mão.");
+
+        op = scan.nextInt();
+
+        if(op == 1){
+            System.out.println("Indique o número do vizinho que pretende subscrever.");
+            int i=1;
+
+            List<Client> aux = node.listNeighbors();
+            for(Client c: aux){
+                System.out.println(i + ". " + c.getUsername() + c.getKey());
+                i++;
+            }
+
+            int v = scan.nextInt();
+            node.addSubscriber(aux.get(v-1));
+            System.out.println("Subscrição realizada com sucesso.");
+
+            //todo: atualizar publicações em cache (diogo ???) -> acho que a função é a mesma que ele tem que fazer, estou em dúvida
+        }
+        else {
+            System.out.println("Indique a chave RSA do nodo que pretende subscrever.");
+            String key = scan.nextLine();
+
+            //todo (sofia): pedir subscritores pela chave
+        }
+
+        storeState(node, fileName);
+    }
+
+    private static void unsubscribe(Node node, String fileName){
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Indique o número do nodo que pretende des-subscrever.");
+        int i=1;
+
+        List<Client> aux = node.listSubscribers();
+        for(Client c: aux){
+            System.out.println(i + ". " + c.getUsername() + c.getKey());
+            i++;
+        }
+
+        int v = scan.nextInt();
+        node.removeSubscriber(aux.get(v-1).getKey());
+
+        storeState(node, fileName);
+    }
+
     public static void main(String[] args) {
         String fileName = "nodeDB";
         String username = args[0];
+        String host = Util.getPublicIp();
         int localport = Integer.parseInt(args[1]);
         Address bootstrapIP = Address.from(args[2]);
         String RSAFile = args[3];
         String RSA = Util.LoadRSAKey(RSAFile);
-        Node node = loadState(username, RSA, localport, fileName);
+        Node node = loadState(username, RSA, host, localport, fileName);
         boolean out = false;
+        int op;
 
         //todo (sofia): apagar posts antigos
 
         Serializer s = new SerializerBuilder().build();
-        ManagedMessagingService ms = NettyMessagingService.builder().withAddress(Address.from(args[0])).build(); //todo (diogo): mudar IP
+        ManagedMessagingService ms = NettyMessagingService.builder().withAddress(Address.from(host, localport)).build();
         ExecutorService es = Executors.newSingleThreadExecutor();
 
         try {
@@ -155,27 +266,30 @@ public class Node implements Serializable {
             e.printStackTrace();
         }
 
-        //todo (salete): menu
+        System.out.println("Bem vindo" + node.getClient().getUsername() + ".");
 
-        System.out.println("Bem vindo" + node.getClient().getUsername());
+        do{
+            op = showMenu();
 
-        while(!out){
-            int op = showMenu();
-
-
+            switch(op){
+                case 1:
+                    post(node, fileName);
+                    break;
+                case 2:
+                    // todo (aula 6 de Maio): timeline
+                    break;
+                case 3:
+                    subscribe(node, fileName);
+                    break;
+                case 4:
+                    unsubscribe(node, fileName);
+                    break;
+            }
         }
-    }
+        while(op != 5);
 
-    private static int showMenu() {
-        Scanner scan = new Scanner(System.in);
-
-        System.out.println("Escolha uma das seguintes opções: ");
-        System.out.println("1. Publicar.");
-        System.out.println("2. Ver timeline.");
-        System.out.println("3. Subscrever vizinho.");
-        System.out.println("4. Desubscrever vizinho.");
-        System.out.println("5. Logout.");
-
-        return scan.nextInt();
+        if(op == 5){
+            return;
+        }
     }
 }
